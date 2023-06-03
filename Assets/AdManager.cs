5dbd5d7f02b1e9b6e3634332e101401f58870aba
Faiqah Shuaib <faiqahshuaib123@gmail.com>
2023-06-03 13:25:45 +0500

    unity assets

---
+++ Assets/AdManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdManager : MonoBehaviour {
+    public Hashtable ads = new Hashtable();
+    
+    void Start() {
+            
+    }
+
+}
---
+++ Assets/Scripts/FloorManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorManager : MonoBehaviour
+{
+    public GameObject[] floor;
+    // Start is called before the first frame update
+    void Start()
+    {
+           floor = new GameObject[32];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
