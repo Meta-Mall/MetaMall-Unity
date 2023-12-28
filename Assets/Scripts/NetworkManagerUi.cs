6e83f650996c80f16961c86b269cf29e31edd55d
Faiqah Shuaib <faiqahshuaib123@gmail.com>
2023-12-28 22:09:42 +0500

    multiplayer implementation on test scene

--- Assets/Scripts/NetworkManagerUi.cs
+++ Assets/Scripts/NetworkManagerUi.cs
@@ -1,22 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class NetworkManagerUi : MonoBehaviour
 {
-    [SerializeField] Button client;
-    [SerializeField] Button server;
-    [SerializeField] Button host;
-
-    private void Awake() {
-
-        server.onClick.AddListener(() => {
-            NetworkManager.Singleton.StartServer();
-        });
-    }
-
+    [SerializeField] TextMeshProUGUI debugText = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,4 +18,35 @@ public class NetworkManagerUi : MonoBehaviour
     {
         
     }
+
+    public void StartHost() {
+        if (NetworkManager.Singleton.StartHost()) {
+            debugText.text = "Host started";
+            Debug.Log("Host started");
+        }
+        else {
+            Debug.Log("Host failed to Start");
+        }
+    }
+
+    public void StartClient() {
+        if (NetworkManager.Singleton.StartClient()) {
+            debugText.text = "Client started";
+            Debug.Log("Client started");
+        }
+        else {
+            Debug.Log("Client failed to Start");
+        }
+    }
+
+    public void StartServer() {
+        if (NetworkManager.Singleton.StartClient()) {
+            debugText.text = "Server started";
+            Debug.Log("Server started");
+        }
+        else {
+            Debug.Log("Server failed to Start");
+        }
+    }
+
 }
